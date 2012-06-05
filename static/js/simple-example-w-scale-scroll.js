3ce19809d294f1a705ec20e0f7de7a78f74192c5
ouhouhsami <samuel.goldszmidt@gmail.com>
2012-06-05 11:43:22 +0200

    refactor first

--- static/js/simple-example-w-scale-scroll.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	var duration
-	$($('audio').get(0))
-	.bind("loadedmetadata", function(){
-		duration = $(this).get(0).duration;
-		var timeline = new Timeline('audio', 'sound_visualisation', duration,{}, 'scale', 'scroll');
-	})
-})
